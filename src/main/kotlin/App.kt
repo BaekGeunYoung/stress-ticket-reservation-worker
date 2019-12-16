b6737abe018e10a855d868d4735d4f9033ab1c8f
BaekGeunYoung <dvmflstm@naver.com>
2019-12-17 01:11:11 +0900

    add dependencies in gradle build file & simple test in main func

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,15 @@
-fun main() {
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 
+fun main() {
+    val deferred = (1..1_000_000).map { n ->
+        GlobalScope.async {
+            n
+        }
+    }
+
+    runBlocking {
+        val sum = deferred.map { it.await().toLong() }.sum()
+    }
 }
