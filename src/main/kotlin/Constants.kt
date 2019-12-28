431da8b7ec2651f6c22dc53ce68e43aaebafb61b
BaekGeunYoung <dvmflstm@naver.com>
2019-12-28 20:38:37 +0900

    전역적으로 사용하는 상수들 Constants 클래스에 분리

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,6 @@
+class Constants {
+    companion object {
+        val numOfProcessors = 100
+        val sqsEndpoint = "https://sqs.ap-northeast-2.amazonaws.com/182756308452/ticket_reservation_data_queue"
+    }
+}
