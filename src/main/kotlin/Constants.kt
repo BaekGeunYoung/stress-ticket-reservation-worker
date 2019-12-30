e7f822bf44f2c704cb4a33d4fa44dd4ebb886eb2
BaekGeunYoung <dvmflstm@naver.com>
2019-12-31 00:41:24 +0900

    Constants 클래스에 상수 추가 : event_name 들

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -2,5 +2,10 @@ class Constants {
     companion object {
         const val numOfProcessors = 100
         const val sqsEndpoint = "https://sqs.ap-northeast-2.amazonaws.com/182756308452/ticket_reservation_data_queue"
+
+        const val LOGIN = "LOGIN"
+        const val LOGOUT = "LOGOUT"
+        const val PAGE_VIEW = "PAGE_VIEW"
+        const val TICKET_RESERVATION = "TICKET_RESERVATION"
     }
 }
