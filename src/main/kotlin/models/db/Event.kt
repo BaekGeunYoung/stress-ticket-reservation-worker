471eaf19fa0c48b805e65f9fbe36bbf50eead9c4
BaekGeunYoung <dvmflstm@naver.com>
2019-12-31 17:22:08 +0900

    LocalDateTime <-> String 간 타입 변환을 위해 @DynamoDBTypeConverted annotation 추가

--- src/main/kotlin/models/db/Event.kt
+++ src/main/kotlin/models/db/Event.kt
@@ -1,6 +1,7 @@
 package models.db
 
 import com.amazonaws.services.dynamodbv2.datamodeling.*
+import models.db.type_converter.LocalDateTimeConverter
 import java.time.LocalDateTime
 
 @DynamoDBTable(tableName = "event")
@@ -15,18 +16,22 @@ data class Event(
 
     @DynamoDBAttribute(attributeName = "last_login_datetime")
     @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.S)
+    @DynamoDBTypeConverted(converter = LocalDateTimeConverter::class)
     var lastLoginDatetime: LocalDateTime? = null,
 
     @DynamoDBAttribute(attributeName = "last_logout_datetime")
     @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.S)
+    @DynamoDBTypeConverted(converter = LocalDateTimeConverter::class)
     var lastLogoutDatetime: LocalDateTime? = null,
 
     @DynamoDBAttribute(attributeName = "last_page_view_datetime")
     @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.S)
+    @DynamoDBTypeConverted(converter = LocalDateTimeConverter::class)
     var lastPageViewDatetime: LocalDateTime? = null,
 
     @DynamoDBAttribute(attributeName = "last_reservation_datetime")
     @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.S)
+    @DynamoDBTypeConverted(converter = LocalDateTimeConverter::class)
     var lastReservationDatetime: LocalDateTime? = null
 ) {
     constructor() : this(event_id = -1, event_name = "")
